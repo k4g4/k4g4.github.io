61fffc656f8d7e220630c298d29831b76678f1cd
Anders <andresdejesus123@gmail.com>
2019-05-27 16:13:16 -0500

    basic cookie clicker with shop and inventory

---
+++ clicker_script.js
@@ -0,0 +1,42 @@
+$(function() {
+    var cookieImage = $('#cookieImage');
+    var cookieCount = $('#cookieCount');
+    var shop = $('#shop');
+    var inventory = $('#inventory');
+
+    var cookies;
+    function updateCookieCount(newCount) {
+        cookies = newCount;
+        cookieCount.text(`Cookies: ${newCount}`);
+    }
+    updateCookieCount(0);
+
+    var shopItems = {oven: 2, worker: 10, factory: 50};
+    var inventoryItems = Object.assign({}, shopItems);
+    Object.keys(inventoryItems).forEach(key => inventoryItems[key] = 0);
+    Object.entries(shopItems).forEach(function([item, cost]) {
+        var shopItem = $(`<div class="item" id="shop_${item}">${item.toUpperCase()}: ${cost} cookies</div>`);
+        shop.append(shopItem);
+        var invItem = $(`<div class="item" id="inv_${item}">${item.toUpperCase()}: 0</div>`);
+        inventory.append(invItem);
+        shopItem.click(function() {
+            if (cookies >= cost) {
+                updateCookieCount(cookies - cost);
+                inventoryItems[item]++;
+                invItem.text(`${item.toUpperCase()}: ${inventoryItems[item]}`);
+            }
+        });
+    });
+
+    cookieImage.click(function(event) {
+        updateCookieCount(cookies + 1);
+
+        //cookie click animation
+        var initWidth = cookie.width();
+        var newWidth = initWidth + 50;
+        var speed = 50;
+        cookieImage.animate({'width': `${newWidth}px`,}, speed)
+                   .animate({'width': `${initWidth}px`,}, speed,
+                            () => cookieImage.width(initWidth));
+    });
+});
